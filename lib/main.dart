1f7e7cb6bfd16ffbc6413ef52848d4da44c21b70
minihant <hantshen2004@yahoo.com.tw>
2022-06-04 16:54:47 +0800

    add windows style widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:serial_port_win32/serial_port_win32.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:provider/provider.dart';
 import 'key.dart';
 import 'dialogLib.dart';
@@ -12,6 +13,7 @@ import 'portLib.dart';
 import 'setting.dart';
 
 bool kDebugMode = true;
+const borderColor = Color(0xFF805306);
 
 void debugPrint(var msg) {
   if (global.isInDebug) {
@@ -21,31 +23,71 @@ void debugPrint(var msg) {
 
 void main() {
   runApp(const MyApp());
+  doWhenWindowReady(() {
+    final win = appWindow;
+    const initialSize = Size(600, 800);
+    win.minSize = initialSize;
+    win.size = initialSize;
+    win.alignment = Alignment.center;
+    win.title = "mosicgo serial";
+    win.show();
+  });
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({key}) : super(key: key);
-  _SerialApp createState() => _SerialApp();
-}
-
-class _SerialApp extends State<MyApp> {
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  // _SerialApp createState() => _SerialApp();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      // title: 'Flutter Demo',
+      // theme: ThemeData(
+      //   primarySwatch: Colors.blue,
+      // ),
+      // home: const MyHomePage(title: 'SerialPort'),
+
+      home: Scaffold(
+        body: WindowBorder(
+          color: borderColor,
+          width: 1,
+          child: Row(
+            children: [const LeftSide(), MyHomePage()],
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'SerialPort'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+// class _SerialApp extends State<MyApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       // title: 'Flutter Demo',
+//       // theme: ThemeData(
+//       //   primarySwatch: Colors.blue,
+//       // ),
+//       // home: const MyHomePage(title: 'SerialPort'),
+//       home: Scaffold(
+//         body: WindowBorder(
+//           color: borderColor,
+//           width: 1,
+//           child: Row(
+//             children: const [LeftSide(), RightSide()],
+//             // children: [MyHomePage()],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
-  final String title;
+class MyHomePage extends StatefulWidget {
+  // const MyHomePage({Key? key, required this.title}) : super(key: key);
+  MyHomePage({Key? key}) : super(key: key);
+  // final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -54,7 +96,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final DialogLib _DialogLib = DialogLib();
   final PortLib _PortLib = PortLib();
-  final Setting _Setting = Setting();
 
   void Fn_startPollingTimer() {
     int counter = 0;
@@ -119,35 +160,197 @@ class _MyHomePageState extends State<MyHomePage> {
     ScreenUtil.init(context, designSize: const Size(600, 800));
     ScreenUtil().setSp(16 * ScreenUtil().scaleWidth);
 
-    return ChangeNotifierProvider(
-      create: (BuildContext context) {},
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            widget.title,
-            style: TextStyle(fontSize: 28.sp),
+    return
+        // ChangeNotifierProvider(
+        //   create: (BuildContext context) {},
+        //   child: Scaffold(
+        // appBar: AppBar(
+        //   title: Text(
+        //     'ttt',
+        //     style: TextStyle(fontSize: 28.sp),
+        //   ),
+        //   actions: <Widget>[
+        //     Row(
+        //       children: [
+        //         Text(global.MCU_ver, style: TextStyle(fontSize: 16.sp)),
+        //       ],
+        //     ),
+        //     SizedBox(width: 20.sp),
+        //     Row(
+        //       children: [
+        //         Text(global.statusText, style: TextStyle(fontSize: 24.sp)),
+        //       ],
+        //     ),
+        //     _Setting.widget_selectPort(context),
+        //   ],
+        // ),
+        Expanded(
+      child: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [backgroundStartColor, backgroundEndColor],
+              stops: [0.0, 1.0]),
+        ),
+        child: Column(children: [
+          WindowTitleBarBox(
+            child: Row(
+              children: [Expanded(child: MoveWindow()), const WindowButtons()],
+            ),
           ),
-          actions: <Widget>[
-            Row(
-              children: [
-                Text(global.MCU_ver, style: TextStyle(fontSize: 16.sp)),
-              ],
-            ),
-            SizedBox(width: 20.sp),
-            Row(
-              children: [
-                Text(global.statusText, style: TextStyle(fontSize: 24.sp)),
-              ],
-            ),
-            _Setting.widget_selectPort(context),
-          ],
-        ),
-        body: Row(
-          children: [
-            widget_writeData(),
-          ],
-        ),
+          widget_writeData(),
+        ]),
       ),
+    );
+  }
+}
+
+const sidebarColor = Color(0xFFF6A00C);
+
+class LeftSide extends StatelessWidget {
+  const LeftSide({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Setting settingLib = Setting();
+    final PortLib _PortLib = PortLib();
+    final DialogLib _DialogLib = DialogLib();
+    return SizedBox(
+        width: 150,
+        child: Container(
+            color: sidebarColor,
+            child: Column(
+              children: [
+                WindowTitleBarBox(child: MoveWindow()),
+                const SizedBox(height: 10),
+                TextButton.icon(
+                  icon: Icon(Icons.info, size: 24.sp),
+                  label: Text('About', style: TextStyle(fontSize: 20.sp)),
+                  style: TextButton.styleFrom(
+                    alignment: Alignment.centerLeft,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    side: const BorderSide(width: 2, color: Colors.black12),
+                    primary: Colors.white,
+                    backgroundColor: Colors.black,
+                  ),
+                  onPressed: () async {
+                    debugPrint(
+                        "App Version: ${_DialogLib.getPackageVersion()}");
+                    _DialogLib.showAppInfo(context);
+                  },
+                ),
+                const SizedBox(height: 10),
+                TextButton.icon(
+                  icon: Icon(Icons.info, size: 24.sp),
+                  label: Text('Open Port', style: TextStyle(fontSize: 20.sp)),
+                  style: TextButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    side: const BorderSide(width: 2, color: Colors.black12),
+                    primary: Colors.white,
+                    backgroundColor: Colors.black,
+                  ),
+                  onPressed: () async {
+                    settingLib.Fn_showPortInfo(context);
+                  },
+                ),
+                const SizedBox(height: 10),
+                TextButton.icon(
+                  icon: Icon(Icons.close, size: 24.sp),
+                  label: Text('Close Port', style: TextStyle(fontSize: 20.sp)),
+                  style: TextButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    // fixedSize: ,
+                    // side: const BorderSide(width: 2, color: Colors.black12),
+                    primary: Colors.white,
+                    backgroundColor: Colors.black,
+                  ),
+                  onPressed: () async {
+                    _PortLib.Fn_closePort();
+                  },
+                ),
+              ],
+            )));
+  }
+}
+
+const backgroundStartColor = Color(0xFFFFD500);
+const backgroundEndColor = Color(0xFFF6A00C);
+
+class RightSide extends StatelessWidget {
+  const RightSide({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [backgroundStartColor, backgroundEndColor],
+              stops: [0.0, 1.0]),
+        ),
+        child: Column(children: [
+          WindowTitleBarBox(
+            child: Row(
+              children: [Expanded(child: MoveWindow()), const WindowButtons()],
+            ),
+          )
+        ]),
+      ),
+    );
+  }
+}
+
+final buttonColors = WindowButtonColors(
+    iconNormal: const Color(0xFF805306),
+    mouseOver: const Color(0xFFF6A00C),
+    mouseDown: const Color(0xFF805306),
+    iconMouseOver: const Color(0xFF805306),
+    iconMouseDown: const Color(0xFFFFD500));
+
+final closeButtonColors = WindowButtonColors(
+    mouseOver: const Color(0xFFD32F2F),
+    mouseDown: const Color(0xFFB71C1C),
+    iconNormal: const Color(0xFF805306),
+    iconMouseOver: Colors.white);
+
+class WindowButtons extends StatefulWidget {
+  const WindowButtons({Key? key}) : super(key: key);
+
+  @override
+  _WindowButtonsState createState() => _WindowButtonsState();
+}
+
+class _WindowButtonsState extends State<WindowButtons> {
+  void maximizeOrRestore() {
+    setState(() {
+      appWindow.maximizeOrRestore();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        MinimizeWindowButton(colors: buttonColors),
+        appWindow.isMaximized
+            ? RestoreWindowButton(
+                colors: buttonColors,
+                onPressed: maximizeOrRestore,
+              )
+            : MaximizeWindowButton(
+                colors: buttonColors,
+                onPressed: maximizeOrRestore,
+              ),
+        CloseWindowButton(colors: closeButtonColors),
+      ],
     );
   }
 }
